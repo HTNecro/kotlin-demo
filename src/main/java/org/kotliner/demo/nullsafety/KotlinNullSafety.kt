e2a1de032f090be58f21487620e6b3e137d3c360
HTNecro <HTNecro@gmail.com>
2018-01-04 22:37:06 +0800

    update null safety demo

--- src/main/java/org/kotliner/demo/nullsafety/KotlinNullSafety.kt
+++ src/main/java/org/kotliner/demo/nullsafety/KotlinNullSafety.kt
@@ -8,10 +8,23 @@ fun getNotNullString(): String {
     return "Hello, Kotlin"
 }
 
+fun printNullableString(value: String?) {
+    println(value)
+}
+
+fun printNotNullString(value: String) {
+    println(value)
+}
+
+
 fun main(args: Array<String>) {
     val nullable = getNullableString()
 
     // println(nullable.length) //编译错误
+
+    printNullableString(nullable)
+
+    // printNotNullString(nullable) //编译错误
 
     nullable?.let { println(it.length) }
 
@@ -23,4 +36,6 @@ fun main(args: Array<String>) {
 
     val notNull = getNotNullString()
     println(notNull.length)
+
+
 }
