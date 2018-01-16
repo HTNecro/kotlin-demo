f30b03c9e5fff9b8cd1d8103595bc6b669083d3f
HTNecro <HTNecro@gmail.com>
2018-01-16 22:48:11 +0800

    operator overloading demo

--- src/main/java/org/kotliner/demo/operator/OperatorDemo.kt
+++ src/main/java/org/kotliner/demo/operator/OperatorDemo.kt
@@ -10,9 +10,21 @@ operator fun Point.minus(other: Point): Point {
     return Point(this.x - other.x, this.y - other.y)
 }
 
+operator fun Point.inc(): Point {
+    return Point(this.x + 1, this.y + 1)
+}
+
+operator fun Point.dec(): Point {
+    return Point(this.x - 1, this.y - 1)
+}
+
 fun main(args: Array<String>) {
-    val a = Point(1, 3)
-    val b = Point(3, 1)
+    var a = Point(1, 3)
+    var b = Point(1, 3)
     println(a + b)
     println(a - b)
+    println(a++)
+    println(b--)
+    println(a == b)
+
 }
