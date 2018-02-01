5abb1c83719fff8a11fbba410ecba2de96d95d86
HTNecro <HTNecro@gmail.com>
2018-02-01 08:43:52 +0800

    updatet function args demo

--- src/main/java/org/kotliner/demo/function/FunctionArgs.kt
+++ src/main/java/org/kotliner/demo/function/FunctionArgs.kt
@@ -1,12 +1,13 @@
 package org.kotliner.demo.function
 
 fun pagingList(param: String, pageNo: Int = 1, pageSize: Int = 10): List<Any> {
+    println("param: $param, pageNo: $pageNo, pageSize $pageSize")
     return emptyList()
 }
 
 fun main(args: Array<String>) {
-    val page1 = pagingList("param")
-    val page2 = pagingList("param", 2, 20)
-    val page3 = pagingList("param", pageNo = 2)
-    val page4 = pagingList(pageNo = 2, pageSize = 20, param = "param")
+    pagingList("parameters")
+    pagingList("parameters", 2, 20)
+    pagingList("parameters", pageNo = 2)
+    pagingList(pageNo = 2, pageSize = 20, param = "parameters")
 }
