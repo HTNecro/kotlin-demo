99d58ea2f700e09231f4ca37f5f932fe6d9a3703
HTNecro <HTNecro@gmail.com>
2018-02-07 21:34:13 +0800

    fix extension function demo

--- src/main/java/org/kotliner/demo/function/ExtensionFunction.kt
+++ src/main/java/org/kotliner/demo/function/ExtensionFunction.kt
@@ -2,11 +2,13 @@ package org.kotliner.demo.function
 
 import org.kotliner.demo.nullsafety.getNullableString
 
-fun String?.isNotEmpty() = this == null || "" == this
+fun String?.isNotEmpty() = this != null && this != ""
 
 fun main(args: Array<String>) {
     val s = getNullableString()
     if (s.isNotEmpty()) {
-        println(s)
+        println("String: $s")
+    } else {
+        println("String is empty")
     }
 }
