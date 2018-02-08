294b9a9908b9f8230448b5f539a25244bb254867
HTNecro <HTNecro@gmail.com>
2018-02-08 09:50:36 +0800

    suspend fun demo

---
+++ src/main/java/org/kotliner/demo/coroutines/CoroutineDemo2.kt
@@ -0,0 +1,28 @@
+package org.kotliner.demo.coroutines
+
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+
+suspend fun fetchData(): String {
+    delay(1000L)// fetch data from remote
+    return ""
+}
+
+suspend fun parseData(originData: String): String {
+    delay(1000L)// parse data
+    return ""
+}
+
+suspend fun saveData(data: String) {
+    delay(100L)// save data
+}
+
+fun main(args: Array<String>) {
+    launch {
+        val originData = fetchData()
+        val data = parseData(originData)
+        saveData(data)
+    }
+    // block main thread to keep JVM alive
+}
+
